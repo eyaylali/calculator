22c88d95ff93c3863264309845113d0da6e1ba8d
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 13:03:04 -0800

    Added quit functionality and functioning length check functions

--- calculator.py
+++ calculator.py
@@ -5,32 +5,79 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 from arithmetic import *
+import sys
 
 def consume_input(entry):
-    list1 = entry.split(" ")
+    list1 = entry.split()
     return list1
 
+def need_three_inputs(list2):
+    input_length = len(list2)
+
+    if input_length == 3:
+        return True
+    else:
+        return False
+
+def need_two_inputs(list2):
+    input_length = len(list2)
+    if input_length == 2:
+        return True
+    else:
+        return False   
 
 def evaluate(list2):
     operator = list2[0]
-    v_1 = int(list2[1])
-    v_2 = int(list2[2])
+    length_test_3 = need_three_inputs(list2)
+    length_test_2 = need_two_inputs(list2)
     if operator == "+":
-        return add(v_1, v_2)
+        if length_test_3 == True:
+            return add(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     elif operator == "-":
-        return subtract(v_1, v_2)
+        if length_test_3 == True:
+            return subtract(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     elif operator == "*":
-        return multiply(v_1, v_2)
+        if length_test_3 == True:
+            return multiply(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     elif operator == "/":
-        return divide(v_1, v_2)
+        if length_test_3 == True:
+            return divide(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     elif operator == "square":
-        return square(v_1)
+        if length_test_2 == True:
+            return square(int(list2[1]))
+        else:
+            return "Please input only one number."
+    
     elif operator == "cube":
-        return cube(v_1)
+        if length_test_2 == True:
+            return cube(int(list2[1]))
+        else:
+            return "Please input only one number."
+    
     elif operator == "pow":
-        return power(v_1, v_2)
+        if length_test_3 == True:
+            return power(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     elif operator == "mod":
-        return mod(v_1, v_2)
+        if length_test_3 == True:
+            return mod(int(list2[1]), int(list2[2]))
+        else:
+            return "Please input two numbers."
+    
     else:
         return "I don't understand!"
 
@@ -38,9 +85,12 @@ def evaluate(list2):
 def main():
     while True:
         string = raw_input("> ")
-        split_string = consume_input(string)
-        output = evaluate(split_string)
-        print output
+        if string == "q":
+           sys.exit()
+        else:
+            main_list = consume_input(string)
+            output = evaluate(main_list)
+            print output
 
 if __name__ == '__main__':
     main()
