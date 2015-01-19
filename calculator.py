38175ea5022390d182611c31fb4bdfc8bf4a4fb2
Erin Yaylali <erinyaylali@gmail.com>
2015-01-19 12:28:57 -0800

    Refactoring calculator.py

--- calculator.py
+++ calculator.py
@@ -1,85 +1,26 @@
-"""
-calculator.py
 
-Using our arithmetic.py file from Exercise02, create the
-calculator program yourself in this file.
-"""
 from arithmetic import *
 import sys
 
-def consume_input(entry):
-    list1 = entry.split()
-    return list1
+def input_check(main_list):
+    correct_entry_dict = {"+":3,"-":3,"*":3,"/":3,"square":2,"cube":2,"pow":3,"mod":3}
 
-def need_three_inputs(list2):
-    input_length = len(list2)
-
-    if input_length == 3:
-        return True
+    if len(main_list) == correct_entry_dict[main_list[0]]:
+        print "Incorrect number of inputs. Please try again."
     else:
-        return False
-
-def need_two_inputs(list2):
-    input_length = len(list2)
-    if input_length == 2:
-        return True
-    else:
-        return False   
+        return main_list
 
 def evaluate(list2):
     operator = list2[0]
-    length_test_3 = need_three_inputs(list2)
-    length_test_2 = need_two_inputs(list2)
-    if operator == "+":
-        if length_test_3 == True:
-            return add(int(list2[1]), int(list2[2]))
+    dict_of_functions = {"+": add,"-":subtract,"*": multiply,"/":divide,"square":square,"cube":cube,"pow":power,"mod":mod}
+
+    if operator in dict_of_functions:
+        if len(list2) == 3:
+            return dict_of_functions[operator](float(list2[1]), float(list2[2]))
         else:
-            return "Please input two numbers."
-    
-    elif operator == "-":
-        if length_test_3 == True:
-            return subtract(int(list2[1]), int(list2[2]))
-        else:
-            return "Please input two numbers."
-    
-    elif operator == "*":
-        if length_test_3 == True:
-            return multiply(int(list2[1]), int(list2[2]))
-        else:
-            return "Please input two numbers."
-    
-    elif operator == "/":
-        if length_test_3 == True:
-            return divide(int(list2[1]), int(list2[2]))
-        else:
-            return "Please input two numbers."
-    
-    elif operator == "square":
-        if length_test_2 == True:
-            return square(int(list2[1]))
-        else:
-            return "Please input only one number."
-    
-    elif operator == "cube":
-        if length_test_2 == True:
-            return cube(int(list2[1]))
-        else:
-            return "Please input only one number."
-    
-    elif operator == "pow":
-        if length_test_3 == True:
-            return power(int(list2[1]), int(list2[2]))
-        else:
-            return "Please input two numbers."
-    
-    elif operator == "mod":
-        if length_test_3 == True:
-            return mod(int(list2[1]), int(list2[2]))
-        else:
-            return "Please input two numbers."
-    
+            return dict_of_functions[operator](float(list2[1]))
     else:
-        return "I don't understand!"
+        return "I don't understand! Please input valid operator."
 
 
 def main():
@@ -88,7 +29,8 @@ def main():
         if string == "q":
            sys.exit()
         else:
-            main_list = consume_input(string)
+            main_list = string.split()
+            input_check(main_list)
             output = evaluate(main_list)
             print output
 
